0db666b47dd45797de001e01cace1407ae156e7c
victor oricha <vicky.robilson5528@gmail.com>
2024-11-22 14:27:42 +0100

    profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,5 +82,23 @@ def logout():
     return redirect("/", code=302)
 
 
+@app.route("/profile", methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """
+    Handles GET requests to the /profile route.
+    Retrieves user information based on session ID.
+
+    Returns:
+        User's email as JSON if session is valid, 403 otherwise.
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
