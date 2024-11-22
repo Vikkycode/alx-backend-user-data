299703a0c3eb8c0f002abdb4f79c6c4777d85a66
victor oricha <vicky.robilson5528@gmail.com>
2024-11-22 14:53:59 +0100

    reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -100,5 +100,20 @@ def profile() -> str:
         abort(403)
 
 
+@app.route("/reset_password", methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    Handles POST requests to /reset_password for generating reset tokens.
+    """
+    email = request.form.get("email")
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        # Forbidden if email not found
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
